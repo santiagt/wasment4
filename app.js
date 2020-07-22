47203009c5ef8eabacb1fcc48c3c6046e956a418
santiagt <santiagotorresredecilla@gmail.com>
2020-07-22 17:50:42 +0300

    changes to appjs

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/src/index.html"));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.get("/about", (req, res) => {
   res.send("This is a test");
 });
 
-app.use("/", express.static(__dirname + "/src"));
+app.use("/", express.static(__dirname + "/"));
 app.listen(8080);
 console.log("server started");
